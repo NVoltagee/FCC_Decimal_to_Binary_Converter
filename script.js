21af87fbea747a97da78178811469446c01bfd26
NVoltagee <natrujillo9@gmail.com>
2024-04-18 17:34:32 -0400

    Add decimalToBinary function for converting decimal numbers to binary.

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-  console.log(numberInput.value);
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
   convertBtn.addEventListener("click", checkUserInput);
   numberInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
       checkUserInput();
     }
   });
+};
+
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
 };
