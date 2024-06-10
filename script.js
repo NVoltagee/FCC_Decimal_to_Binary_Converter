3d770b28414146d22633215e78064b22038ef266
Norman Trujillo <natrujillo9@gmail.com>
2024-06-10 19:22:27 -0400

    - Update paragraph elements with msg text after showMsgDelay using setTimeout. - Remove paragraph elements after removeElDelay using setTimeout. - Update result text content to decimalToBinary(5) after 20000 ms using setTimeout.

--- script.js
+++ script.js
@@ -45,7 +45,15 @@ const showAnimation = () => {
       animationContainer.innerHTML += `
       <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px" class="animation-frame">decimalToBinary(${obj.inputVal})</p>`;
     }, obj.addElDelay);
-    setTimeout(() => {}, obj.showMsgDelay);
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).textContent = obj.msg;
+    }, obj.showMsgDelay);
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).remove();
+    }, obj.removeElDelay);
+    setTimeout(() => {
+      result.textContent = decimalToBinary(5);
+    }, 20000);
   });
 };
 
